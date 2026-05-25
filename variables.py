04e03a84167bd5e45b95e46e4615c90304cc58e5
Dundy-369 <dundypadmatejuteju@gmail.com>
2026-05-25 21:32:10 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+# Integer variable
+age = 20
+
+# String variable
+name = "Teja"
+
+# Float variable
+height = 5.8
+
+# Boolean variable
+is_student = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Changing variable values
+age = age + 1
+height = height + 0.1
+
+print("\nAfter updating:")
+print("Age:", age)
+print("Height:", height)
+
+# Combining variables
+message = name + " is " + str(age) + " years old."
+print(message)
